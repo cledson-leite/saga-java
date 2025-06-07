b09b5845fdd848de04e5434c04e707fa8c683776
cledson leite <40147313+cledson-leite@users.noreply.github.com>
2025-06-07 12:52:52 -0300

    include saga coreografado

---
+++ coreografado/src/main/java/springboot/saga/coreografado/CoreografadoApplication.java
@@ -0,0 +1,13 @@
+package springboot.saga.coreografado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreografadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreografadoApplication.class, args);
+	}
+
+}
---
+++ coreografado/src/test/java/springboot/saga/coreografado/CoreografadoApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.saga.coreografado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreografadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
